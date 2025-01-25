8b12d0f0d8bc976fa173a3741e86858765e1662c
khaitr23 <khaitr23@cs.washington.edu>
2025-01-25 14:44:28 -0800

    added lib folder and neo4js driver

---
+++ lib/neo4j.tsx
@@ -0,0 +1,8 @@
+import neo4j from "neo4j-driver";
+
+const driver = neo4j.driver(
+  "neo4j+s://62eb7145.databases.neo4j.io",
+  neo4j.auth.basic("neo4j", "618HpDJ8LN0CtFCRq_lDf0KKF6CK8F8vQU-p9efJsvQ")
+);
+
+export default driver;
